bc81a81eeec404a1672f048b0c072f3a4b4b85df
diduladishan <diduladishan14@gmail.com>
2024-01-29 16:54:29 +0530

    update

--- JavaScript/parallax.js
+++ JavaScript/parallax.js
@@ -29,3 +29,28 @@ document.addEventListener("DOMContentLoaded", () => {
     hero15.style.left = value * 1.5 + "px";
   });
 });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   let hero2 = document.getElementById("hero2");
+//   let hero4 = document.getElementById("hero4");
+//   let hero5 = document.getElementById("hero5");
+//   let hero8 = document.getElementById("hero8");
+//   let hero9 = document.getElementById("hero9");
+//   let hero15 = document.getElementById("hero15");
+//   let hero14 = document.getElementById("hero14");
+
+//   window.addEventListener("scroll", () => {
+//     let value = window.scrollY;
+
+//     hero8.style.left = value * -2.5 + "px";
+//     hero8.style.top = value * 0.5 + "px";
+//     hero9.style.left = value * 0.5 + "px";
+//     hero2.style.left = value * -0.5 + "px";
+//     hero14.style.marginTop = value * 3 + "px";
+//     hero9.style.left = value * 0.5 + "px";
+//     hero2.style.left = value * -0.5 + "px";
+//     hero4.style.left = value * -1.5 + "px";
+//     hero5.style.top = value * -1.5 + "px";
+//     hero15.style.left = value * 1.5 + "px";
+//   });
+// });
