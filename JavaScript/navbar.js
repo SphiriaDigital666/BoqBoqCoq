c7277f9bd2f65903e21a65d0e065ba5562dfd8e9
diduladishan <diduladishan14@gmail.com>
2024-02-03 13:12:15 +0530

    loader updated

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -1,3 +1,11 @@
 function changeBackgroundColor(color) {
   document.querySelector(".helloo").style.backgroundColor = color;
 }
+
+// loader starts here
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
+// loader ends here
